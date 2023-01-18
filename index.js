3e04c71e6597b97c7005a1c4879df494e463d910
Tanyasynytska <synytska3d@gmail.com>
2023-01-18 17:36:06 +0000

    temperaturecurrent

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+function temperatureNow(response){
+console.log(response.data)
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+
+
+temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+
+}
+
+let apiKey = "0d433b84a5fd6bae98118c5f85896fad";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Kyiv&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(temperatureNow);
+
+
 function dateTime() {
         let now = new Date();
         let day = now.getDay();
@@ -60,3 +83,6 @@ function dateTime() {
 
 
  let searchCityFrom = document.querySelector("#search");
+
+  
+ 
